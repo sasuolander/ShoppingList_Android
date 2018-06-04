a572e3e681f97c882e499b45341220c077521230
sasuolander <sasuolander@yahoo.com>
2018-06-04 13:34:53 +0300

    init

---
+++ app/src/main/java/com/sasu/shoppinglist/ShoppingList.kt
@@ -0,0 +1,12 @@
+package com.sasu.shoppinglist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoppingList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_list)
+    }
+}
